a7ff19d7b10ff529b2728967d652d424176dc403
01B <no.pain.no.code@gmail.com>
2015-11-16 04:31:44 +0900

    add Factorial class

---
+++ src/net/nopainnocode/dsaa/algorithms/Factorial.java
@@ -0,0 +1,27 @@
+package net.nopainnocode.dsaa.algorithms;
+
+import java.util.Scanner;
+
+/**
+ * Created by no_pain_no_code on 2015. 11. 16..
+ */
+public class Factorial {
+
+    public static int doFactorial(int i) {
+
+        if (1 == i)
+            return 1;
+        else
+            return i * doFactorial(--i);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.printf("Enter the N value : ");
+        int n = new Scanner(System.in).nextInt();
+
+        System.out.printf("The factorial value of " + n + " is " + doFactorial(n));
+    }
+
+
+}
