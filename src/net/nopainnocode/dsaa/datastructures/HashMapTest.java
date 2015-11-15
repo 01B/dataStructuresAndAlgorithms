72d6373618cd77f0c0fbfb88223d9ba45b5bf3fe
01B <no.pain.no.code@gmail.com>
2015-11-16 04:27:19 +0900

    Hashmap 키 중복 테스트

--- src/net/nopainnocode/dsaa/datastructures/HashMapTest.java
+++ src/net/nopainnocode/dsaa/datastructures/HashMapTest.java
@@ -1,7 +1,6 @@
 package net.nopainnocode.dsaa.datastructures;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by no_pain_no_code on 2015. 11. 15..
@@ -10,21 +9,47 @@ public class HashMapTest {
 
     private Map hashMap = new HashMap();
 
-    public int addDefaultData(){
+    public Map getHashMap(){
+        return this.hashMap;
+    }
 
-        this.hashMap.put(1,"a");
-        this.hashMap.put(2,"b");
-        this.hashMap.put(3,"c");
-        this.hashMap.put(1,"d");
-        this.hashMap.put(2,"d");
-        this.hashMap.put(4,"d");
+    public int putTestData() {
+
+        this.hashMap.put(null, null);
+        this.hashMap.put(1, "b");
+        this.hashMap.put(2, "c");
+        this.hashMap.put(3, "d");
+        this.hashMap.put(1, "d");
+        this.hashMap.put(4, "d");
 
         return this.hashMap.size();
     }
 
-    public void testPrintTheReturnValue() {
+    public Object getReturnValueOfPuttingExistKeyValue() {
 
-        System.out.printf("The return value of adding redundant key value is "
-                + this.hashMap.put(1, "e"));
+        return this.hashMap.put(1, "e");
     }
+
+    public static void main(String[] args) {
+
+        HashMapTest hashMapTest = new HashMapTest();
+        System.out.printf("The size of the HashMap is "
+                + hashMapTest.putTestData());
+
+        // return the before value
+        System.out.printf("\nThe return value of putting exist key value is "
+                + hashMapTest.getReturnValueOfPuttingExistKeyValue());
+
+        Set keySet = hashMapTest.getHashMap().keySet();
+        Iterator iter = keySet.iterator();
+
+        while(iter.hasNext()) {
+
+            Object key = iter.next();
+            Object value = hashMapTest.getHashMap().get(key);
+
+            System.out.printf("\nkey : " + key + " value : " + value);
+        }
+    }
+
 }
